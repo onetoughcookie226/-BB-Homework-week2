029e059473a50a1e5df4cfe299e32e6aee6e9f49
onetoughcookie226 <onetoughcookie226@gmail.com>
2018-09-22 12:51:39 +0200

    Create a modual names ShoppingCart, create new class Cart and export teh class

---
+++ src/ShoppingCart.js
@@ -0,0 +1,22 @@
+//create a module named `ShoppingCart`. 
+//The module must export a `class`, as its default export!
+
+module.exports = class Cart {
+    constructor(name, quantity, price) {
+        this.name = name;
+        this.quantity = quantity;
+        this.price = price;
+    }
+    
+  }
+
+
+
+  //Create new Cart instance
+//   const myCart = new Cart('', 2, 2);
+
+
+
+
+// var shoppingCard = []
+// cart.getItems()
